a0acd7bbc94f1ce4890b896de4b07abf7ffae093
DaffaPutra7 <putradika3604@gmail.com>
2023-05-22 11:28:21 +0700

    class orang [2]

--- Pewarisan/Pewarisan.cpp
+++ Pewarisan/Pewarisan.cpp
@@ -6,5 +6,13 @@ class orang {
 public:
 	string nama;
 
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+
 
 };
